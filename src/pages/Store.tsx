46cacbdb70057242170bf95d8cdc33dc65672ff5
Sashi Kumar Patel <61133962+skpayaya@users.noreply.github.com>
2022-06-29 13:07:41 +0530

    Store Page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,6 @@
 import { Col, Row } from "react-bootstrap";
 import storeItems from "../data/items.json";
+import { StoreItem } from "../components/StoreItem";
 
 export function Store() {
   return (
@@ -7,7 +8,9 @@ export function Store() {
       <h1>Store</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
         {storeItems.map((item) => (
-          <Col></Col>
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
         ))}
       </Row>
     </>
